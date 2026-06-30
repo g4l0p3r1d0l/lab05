defadd0560e71acd95c23a7c0e6a1ab49bbce772
g4l0p3r1d0l <utockakra916@gmail.com>
2026-06-30 18:05:00 +0300

    feat: completed lab05 homework with 100% coverage

---
+++ _build/tests/CMakeFiles/check.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check.dir/test_account.cpp.o"
+  "CMakeFiles/check.dir/test_account.cpp.o.d"
+  "CMakeFiles/check.dir/test_transaction.cpp.o"
+  "CMakeFiles/check.dir/test_transaction.cpp.o.d"
+  "check"
+  "check.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
